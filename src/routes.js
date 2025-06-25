3922e68c18864813ce9a5ae115586c510d6dfa68
Tiago Baptista Noronha <tiago.bn@gmail.com>
2025-06-25 10:59:52 -0300

    feature: extract route params

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,32 @@
+import { buildRoutePath } from "./util/build-route-path.js";
 
 
 
 export const routes = [
   {
     method: 'POST',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => res.end("Rota não implementada")
 
   },
   {
     method: 'GET',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => res.end("Rota não implementada")
   },
   {
     method: 'PUT',
-    path: '/tasks/:id',
-    handler: (req, res) => res.end("Rota não implementada")
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => res.end(JSON.stringify({ method: 'put', params: req.params }))
   },
   {
     method: 'DELETE',
-    path: '/tasks/:id',
-    handler: (req, res) => res.end("Rota não implementada")
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => res.end(JSON.stringify({ method: 'delete', params: req.params }))
   },
   {
     method: 'PATCH',
-    path: '/tasks/:id/complete',
-    handler: (req, res) => res.end("Rota não implementada")
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => res.end(JSON.stringify({ method: 'patch', params: req.params }))
   },
 ]
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import http from 'node:http'
 import { routes } from './routes.js';
 import { json } from './middlewares/json.js';
+import { Database } from './database.js';
+import { buildRoutePath } from './util/build-route-path.js';
+
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -9,10 +13,16 @@ const server = http.createServer(async (req, res) => {
 
   const route = routes.find(
     route =>
-      route.method === method && route.path === url
+      route.method === method && route.path.test(url)
   );
 
-  if (route) return route.handler(req, res)
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    req.params = { ...routeParams.groups }
+
+    return route.handler(req, res)
+  }
 
   return res.writeHead(404).end();
 })
